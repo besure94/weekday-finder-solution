2011976121cf0e37b3ab26b3beeed6645de7dfb5
Brian Scherner <brianscherner@gmail.com>
2023-09-21 13:12:58 -0700

    Add Month get functionality + passing test.

--- WeekdayFinder.Tests/ModelTests/DateTests.cs
+++ WeekdayFinder.Tests/ModelTests/DateTests.cs
@@ -10,8 +10,17 @@ namespace WeekdayFinder.Tests
     [TestMethod]
     public void DateConstructor_CreatesInstanceOfDate_Date()
     {
-      Date newDate = new Date();
+      Date newDate = new Date(9);
       Assert.AreEqual(typeof(Date), newDate.GetType());
+    }
+
+    [TestMethod]
+    public void GetMonth_ReturnsMonth_Int()
+    {
+      int month = 9;
+      Date newDate = new Date(month);
+      int result = newDate.Month;
+      Assert.AreEqual(month, result);
     }
 
   }
--- WeekdayFinder/Models/Date.cs
+++ WeekdayFinder/Models/Date.cs
@@ -3,9 +3,15 @@ namespace WeekdayFinder.Models
 
   public class Date
   {
-    public Date()
+    private int _month;
+    public int Month
     {
+      get { return _month; }
+    }
 
+    public Date(int month)
+    {
+      _month = month;
     }
   }
 }
